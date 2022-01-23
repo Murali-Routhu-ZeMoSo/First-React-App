0a6aa62d2fc8bc9f07d4363933f3e8bf86fbd74e
Murali Krishna <muralikrishnarouthu1818@gmail.com>
2022-01-23 20:42:29 +0530

    added padding to ingredients span element

--- src/components/Ingredient.js
+++ src/components/Ingredient.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Ingredient({ amount, name }) {
   return (
     <>
-      <span>{name}</span>
+      <span style={{ paddingRight: "10px" }}>{name}</span>
       <span>{amount}</span>
     </>
   );
